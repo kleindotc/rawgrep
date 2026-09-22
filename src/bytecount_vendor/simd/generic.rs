4cf9186d330072317063a7e6f31724c09ba813b1
Mark Tyrkba <awfulmarco@gmail.com>
2026-09-22 21:06:51 +0300

    fix: count accumulation in the bytecount vendor code

--- src/bytecount_vendor/simd/generic.rs
+++ src/bytecount_vendor/simd/generic.rs
@@ -45,7 +45,7 @@ pub fn chunk_count(haystack: &[u8], needle: u8) -> usize {
         while haystack.len() >= offset + 64 * 255 {
             let mut counts = u8x64::splat(0);
             for _ in 0..255 {
-                counts -= u8x64_from_offset(haystack, offset).simd_eq(needles_x64).select(Simd::splat(1u8), Simd::splat(0u8));
+                counts += u8x64_from_offset(haystack, offset).simd_eq(needles_x64).select(Simd::splat(1u8), Simd::splat(0u8));
                 offset += 64;
             }
             count += sum_x64(&counts);
@@ -55,7 +55,7 @@ pub fn chunk_count(haystack: &[u8], needle: u8) -> usize {
         if haystack.len() >= offset + 64 * 128 {
             let mut counts = u8x64::splat(0);
             for _ in 0..128 {
-                counts -= u8x64_from_offset(haystack, offset).simd_eq(needles_x64).select(Simd::splat(1u8), Simd::splat(0u8));
+                counts += u8x64_from_offset(haystack, offset).simd_eq(needles_x64).select(Simd::splat(1u8), Simd::splat(0u8));
                 offset += 64;
             }
             count += sum_x64(&counts);
@@ -66,7 +66,7 @@ pub fn chunk_count(haystack: &[u8], needle: u8) -> usize {
         // 32
         let mut counts = u8x32::splat(0);
         for i in 0..(haystack.len() - offset) / 32 {
-            counts -=
+            counts +=
                 u8x32_from_offset(haystack, offset + i * 32).simd_eq(needles_x32).select(Simd::splat(1u8), Simd::splat(0u8));
         }
         count += sum_x32(&counts);
@@ -74,7 +74,7 @@ pub fn chunk_count(haystack: &[u8], needle: u8) -> usize {
         // Straggler; need to reset counts because prior loop can run 255 times
         counts = u8x32::splat(0);
         if haystack.len() % 32 != 0 {
-            counts -=
+            counts +=
                 u8x32_from_offset(haystack, haystack.len() - 32).simd_eq(needles_x32).select(Simd::splat(1u8), Simd::splat(0u8))
                     & u8x32_from_offset(&MASK, haystack.len() % 32);
         }
@@ -103,7 +103,7 @@ pub fn chunk_num_chars(utf8_chars: &[u8]) -> usize {
         while utf8_chars.len() >= offset + 64 * 255 {
             let mut counts = u8x64::splat(0);
             for _ in 0..255 {
-                counts -= is_leading_utf8_byte_x64(u8x64_from_offset(utf8_chars, offset));
+                counts += is_leading_utf8_byte_x64(u8x64_from_offset(utf8_chars, offset));
                 offset += 64;
             }
             count += sum_x64(&counts);
@@ -113,7 +113,7 @@ pub fn chunk_num_chars(utf8_chars: &[u8]) -> usize {
         if utf8_chars.len() >= offset + 64 * 128 {
             let mut counts = u8x64::splat(0);
             for _ in 0..128 {
-                counts -= is_leading_utf8_byte_x64(u8x64_from_offset(utf8_chars, offset));
+                counts += is_leading_utf8_byte_x64(u8x64_from_offset(utf8_chars, offset));
                 offset += 64;
             }
             count += sum_x64(&counts);
@@ -122,14 +122,14 @@ pub fn chunk_num_chars(utf8_chars: &[u8]) -> usize {
         // 32
         let mut counts = u8x32::splat(0);
         for i in 0..(utf8_chars.len() - offset) / 32 {
-            counts -= is_leading_utf8_byte_x32(u8x32_from_offset(utf8_chars, offset + i * 32));
+            counts += is_leading_utf8_byte_x32(u8x32_from_offset(utf8_chars, offset + i * 32));
         }
         count += sum_x32(&counts);
 
         // Straggler; need to reset counts because prior loop can run 255 times
         counts = u8x32::splat(0);
         if utf8_chars.len() % 32 != 0 {
-            counts -=
+            counts +=
                 is_leading_utf8_byte_x32(u8x32_from_offset(utf8_chars, utf8_chars.len() - 32))
                     & u8x32_from_offset(&MASK, utf8_chars.len() % 32);
         }
